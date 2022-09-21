b10d9ea96712dad0f3ecbefd21db87b9e0a4d517
Fenil <pfenil@my.centennialcollege.ca>
2022-09-20 22:00:19 -0400

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const exp = require("express");
 const app = exp();
 const path = require("path");
-const bodyparser = require("body-parser");
+
+const bodyparser = require("body-parser"); // body-parser is used to take text from the body or post() while we can have value from get easily.  
+app.use(bodyparser.urlencoded({extended: true}));
+//app.use(bodyparser.json()); 
 
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname,"home.html"));
-})
+});
 
 app.get("/about_us", function(req,res){
     res.sendFile(path.join(__dirname,"about_us.html"));
-})
+});
 
 /*app.get("/", function(req,res){
     res.send("text message");
@@ -22,24 +25,24 @@ app.get("/about_us", function(req,res){
 })*/
 app.get("/login" , function(req,res){
     res.sendFile(path.join(__dirname,"login.html"));
-})
+});
 app.get("/user_login" , function(req,res){
     var username= req.query.username;
     var password = req.query.password;
     res.send("Username: " + username + ", password : " + password);
-})
+});
 app.post("/user_login" , function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-    res.redirect("/admin_dashboard");
-    //res.send("Username: " + username + ", password : " + password);
+    //res.redirect("/admin_dashboard");
+    res.send("POST -> Username: " + username + ", password : " + password);
     //res.send("POST: login");  
-}) 
+}); 
 app.get("/admin_dashboard" , function(req,res){
     //res.send("text message from admin ")
     var obj = {username: "Fenil" , password: "545"};
     res.json(obj);
-})
+});
 //localhost:3000/product/ 14578
 app.get("/product/:id",function(req,res){
     var product_id = req.params.id;
